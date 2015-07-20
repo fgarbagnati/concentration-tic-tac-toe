753667f2e253aecdcb8c5ef7287e0a7e1eef52a7
Mia Garbagnati <f.g.garbagnati@gmail.com>
2015-07-20 16:17:01 -0700

    adds passing test woooohoo

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,6 +2,11 @@ require "spec_helper"
 
 module ConcentrationTicTacToe
   describe Cell do
-    # tests will be added here
+    context "#initialize" do
+      it "is initialized with a value of '' by default" do
+        cell = Cell.new
+        expect(cell.value).to eq ''
+      end
+    end
   end
 end
